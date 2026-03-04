bb5353c1e91b2014614fffbfbde5700f985b8ee5
lesterjohnpama <lesterjohnpama1@gmail.com>
2026-03-04 16:20:10 +0800

    Add index.php with welcome message and layout

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include "header.php"; ?>
+
+<div class="text-center">
+    <h1>Welcome to My Website</h1>
+    <p class="lead">Modern PHP Website with Login System</p>
+</div>
+
+<?php include "footer.php"; ?>
